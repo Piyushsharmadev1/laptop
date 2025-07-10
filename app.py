5966b726b23bd303f12a04f5a39bd816d1d9260b
Piyush <piyushsharmahero22000@gmail.com>
2025-07-10 18:22:16 +0530

    added

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Load the model and dataset
+pipe = pickle.load(open('pipe.pkl', 'rb'))
+df = pickle.load(open('df.pkl', 'rb'))
+
+st.title("💻 Laptop Price Predictor")
+
+# Brand
+company = st.selectbox('Brand', df['Company'].unique())
+
+# Type
+laptop_type = st.selectbox('Type', df['TypeName'].unique())
+
+# RAM
+ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# Weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS Display', ['No', 'Yes'])
+
+# Screen Size
+screen_size = st.slider('Screen Size (inches)', 10.0, 18.0, 13.0)
+
+# Resolution
+resolution = st.selectbox(
+    'Screen Resolution',
+    ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440']
+)
+
+# CPU
+cpu = st.selectbox('CPU', df['Cpu brand'].unique())
+
+# HDD
+hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
+
+# SSD
+ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
+
+# GPU
+gpu = st.selectbox('GPU Brand', df['Gpu brand'].unique())
+
+# OS
+os = st.selectbox('Operating System', df['os'].unique())
+
+if st.button('Predict Price'):
+    # Convert to binary
+    touchscreen = 1 if touchscreen == 'Yes' else 0
+    ips = 1 if ips == 'Yes' else 0
+
+    # Calculate PPI
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res ** 2 + Y_res ** 2) ** 0.5) / screen_size
+
+    # Build query array
+    query = np.array([company, laptop_type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+    query = query.reshape(1, 12)
+
+    # Predict
+    predicted_price = int(np.exp(pipe.predict(query)[0]))
+    st.success(f"💰 The predicted price of this configuration is ₹ {predicted_price}")
